fca27f71248a0f098fa84542068193c99b2d4ed0
FreelanceWillie <funkmonk64@gmail.com>
2026-07-13 15:01:01 -0500

    Deploy: hand-card wrap fix, all --qa targets pass (eb73fc3)

--- index.js
+++ index.js
@@ -533,7 +533,7 @@ const InternalConfig = function (initConfig) { // eslint-disable-line no-unused-
 			'printErr': this.onPrintError,
 			'thisProgram': this.executable,
 			'noExitRuntime': false,
-			'dynamicLibraries': [`${loadPath}.side.wasm?v=dc352bc`].concat(this.gdextensionLibs),
+			'dynamicLibraries': [`${loadPath}.side.wasm?v=eb73fc3`].concat(this.gdextensionLibs),
 			'emscriptenPoolSize': this.emscriptenPoolSize,
 			'instantiateWasm': function (imports, onSuccess) {
 				function done(result) {
@@ -553,17 +553,17 @@ const InternalConfig = function (initConfig) { // eslint-disable-line no-unused-
 				if (!path.startsWith('godot.')) {
 					return path;
 				} else if (path.endsWith('.audio.worklet.js')) {
-					return `${loadPath}.audio.worklet.js?v=dc352bc`;
+					return `${loadPath}.audio.worklet.js?v=eb73fc3`;
 				} else if (path.endsWith('.audio.position.worklet.js')) {
-					return `${loadPath}.audio.position.worklet.js?v=dc352bc`;
+					return `${loadPath}.audio.position.worklet.js?v=eb73fc3`;
 				} else if (path.endsWith('.js')) {
 					return `${loadPath}.js`;
 				} else if (path in gdext) {
 					return path;
 				} else if (path.endsWith('.side.wasm')) {
-					return `${loadPath}.side.wasm?v=dc352bc`;
+					return `${loadPath}.side.wasm?v=eb73fc3`;
 				} else if (path.endsWith('.wasm')) {
-					return `${loadPath}.wasm?v=dc352bc`;
+					return `${loadPath}.wasm?v=eb73fc3`;
 				}
 				return path;
 			},
@@ -666,7 +666,7 @@ const Engine = (function () {
 	Engine.load = function (basePath, size) {
 		if (loadPromise == null) {
 			loadPath = basePath;
-			loadPromise = preloader.loadPromise(`${loadPath}.wasm?v=dc352bc`, size, true);
+			loadPromise = preloader.loadPromise(`${loadPath}.wasm?v=eb73fc3`, size, true);
 			requestAnimationFrame(preloader.animateProgress);
 		}
 		return loadPromise;
@@ -820,7 +820,7 @@ const Engine = (function () {
 				this.config.update(override);
 				// Add main-pack argument.
 				const exe = this.config.executable;
-				const pack = this.config.mainPack || `${exe}.pck?v=dc352bc`;
+				const pack = this.config.mainPack || `${exe}.pck?v=eb73fc3`;
 				this.config.args = ['--main-pack', pack].concat(this.config.args);
 				// Start and init with execName as loadPath if not inited.
 				const me = this;
